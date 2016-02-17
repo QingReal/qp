73f16c00ef98d18a0df75f1a570e2f7d02748cd9
QingReal <857617647@qq.com>
2016-02-17 14:00:41 +0800

    project first init by qing

---
+++ qp-core/src/main/java/com/qp/core/App.java
@@ -0,0 +1,13 @@
+package com.qp.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
